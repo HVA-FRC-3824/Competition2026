a5913444919f6d00712f13edd9e747f9cb0a50f7
Jackson Case <casejack100@gmail.com>
2026-04-01 22:25:55 -0400

    Add optimization

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Competition2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 103;
-  public static final String GIT_SHA = "2be871f2a46ccee0cb7fdf88a9ffedee5b14d6ac";
-  public static final String GIT_DATE = "2026-04-01 21:37:41 EDT";
+  public static final int GIT_REVISION = 104;
+  public static final String GIT_SHA = "dc4b4288856cfcfad5bf78794f2665eb30a28fb0";
+  public static final String GIT_DATE = "2026-04-01 22:19:30 EDT";
   public static final String GIT_BRANCH = "java";
-  public static final String BUILD_DATE = "2026-04-01 21:55:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1775094939942L;
+  public static final String BUILD_DATE = "2026-04-01 22:20:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1775096428440L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
